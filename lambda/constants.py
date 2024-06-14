eb29b94018b464f734d0d37479be1adf623fddbe
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-06-14 11:24:25 -0400

    commit

---
+++ lambda/constants.py
@@ -0,0 +1,5 @@
+# Constants File
+
+LATITUDE = "45.334904"
+LONGITUDE = "-75.724098"
+API_KEY = your_open_weather_map_api_key
--- secrets.py
+++
@@ -1,5 +0,0 @@
-# Nothing to see here
-# Fill in the blanks to use the program
-LATITUDE = latitude_of_your_location
-LONGITUDE = longitude_of_your_location
-API_KEY = your_open_weather_map_api_key
